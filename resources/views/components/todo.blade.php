45afee4ed918031282dfbf883f8838da3553977e
Jairus Joer <hello@jairusjoer.com>
2024-07-09 22:17:50 +0200

    Add basic todo component

---
+++ resources/views/components/todo.blade.php
@@ -0,0 +1,9 @@
+<div class="flex gap-4 border rounded-xl p-4 bg-zinc-100">
+    <form action="">
+        <input type="text" placeholder="Your todo...">
+        <button>Add</button>
+    </form>
+    <button>Undo</button>
+    <button>Done</button>
+    <button class="ml-auto">Delete</button>
+</div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,16 @@
-
 @extends('layouts.root')
 
 @section('root')
-<h1 class="text-red-500">Tailwind</h1>
+    <main class="py-12 max-w-xl mx-auto">
+        <header>
+            <h1 class="font-medium">Todos</h1>
+        </header>
+        <section>
+            <x-todo />
+        </section>
+        <section class="mt-12">
+            <h2 class="font-medium">Done</h2>
+            <x-todo />
+        </section>
+    </main>
 @endsection
