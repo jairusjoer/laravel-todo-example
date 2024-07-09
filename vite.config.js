67e55ab01d4a30c9e5195a27e30e9c923280d644
Jairus Joer <hello@jairusjoer.com>
2024-07-09 22:02:02 +0200

    Add Tailwind CSS

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+
+// PostCSS plugins
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/globals.css", "resources/js/globals.js"],
             refresh: true,
         }),
     ],
+    css: {
+        postcss: {
+            plugins: [tailwindcss, autoprefixer],
+        },
+    },
 });
