b252f0cecf9a909a0ec451b144c09140f80cce01
Jairus Joer <hello@jairusjoer.com>
2024-07-09 22:11:12 +0200

    Add layout using template inheritance

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+@extends('layouts.root')
 
-        @vite(['resources/css/globals.css', 'resources/js/globals.js'])
-    </head>
-    <body class="font-sans antialiased dark:bg-black dark:text-white/50">
-        <h1 class="text-red-500">Tailwind</h1>
-    </body>
-</html>
+@section('root')
+<h1 class="text-red-500">Tailwind</h1>
+@endsection
---
+++ resources/views/layouts/root.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="bg-background text-muted-foreground scroll-smooth">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    @vite(['resources/css/globals.css', 'resources/js/globals.js'])
+</head>
+
+<body class="text-base font-sans antialiased">
+    <div id="root" class="">
+        @yield('root')
+    </div>
+</body>
+
+</html>
