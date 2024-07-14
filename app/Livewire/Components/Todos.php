52bed4e2c3aead4a1353bbf7489725b8c62809ec
Jairus Joer <hello@jairusjoer.com>
2024-07-14 13:50:50 +0200

    Improve fallback behavior and formatting

--- app/Livewire/Components/Todos.php
+++ app/Livewire/Components/Todos.php
@@ -10,13 +10,13 @@ use Livewire\Attributes\On;
 class Todos extends Component
 {
     public Collection $todos;
-    public array $where;
+    public array $where = [];
 
     #[On(['todo.delete', 'todo.done', 'todo.undo'])]
     public function render()
     {
         $this->todos = Todo::where(...$this->where)->get();
+
         return view('livewire.components.todos');
     }
-
 }
