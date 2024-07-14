9989602a9e3ac5a0712684df3ba5091a83fbbf3c
Jairus Joer <hello@jairusjoer.com>
2024-07-14 19:07:13 +0200

    Add schedule to delete done todos

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,18 @@
 <?php
 
+use App\Models\Todo;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Schedule::call(function () {
+    DB::table('todos')
+        ->where('done', 1)
+        ->where('created_at', '<', now()->subDays(7))
+        ->delete();
+})->daily();
