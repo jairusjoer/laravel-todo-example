899551d0abe7bd94e089210421e0df23a37f98c5
Jairus Joer <hello@jairusjoer.com>
2024-07-14 23:08:21 +0200

    Move query to index page and pass as reactive data

--- resources/views/livewire/components/todo/table.blade.php
+++ resources/views/livewire/components/todo/table.blade.php
@@ -2,22 +2,12 @@
 use App\Models\Todo;
 use function Livewire\Volt\{state, on};
 
-$fetch = fn() => ($this->todos = Todo::where(...$this->where)->get()->reverse());
+state(['data' => []])->reactive();
 
-state([
-    'where' => [],
-    'todos' => $fetch,
-]);
-
-on([
-    'todo.create' => $fetch,
-    'todo.update' => $fetch,
-    'todo.delete' => $fetch,
-]);
 ?>
 
 <div class="space-y-3">
-    @foreach ($todos as $todo)
+    @foreach ($data as $todo)
         <livewire:components.todo.item :$todo :key="$todo->id" />
     @endforeach
 </div>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,18 +1,45 @@
+<?php
+use App\Models\Todo;
+use function Livewire\Volt\{state, mount, on};
+
+$fetch = function () {
+    $query = Todo::all()->reverse()->groupBy('done');
+
+    $this->open = $query->get(0);
+    $this->done = $query->get(1);
+};
+
+state([
+    'open' => [],
+    'done' => [],
+]);
+
+mount($fetch);
+
+on([
+    'todo.create' => $fetch,
+    'todo.update' => $fetch,
+    'todo.delete' => $fetch,
+]);
+?>
+
 @extends('layouts.root')
 
 @section('root')
-    <main class="py-12 max-w-xl mx-auto">
-        <header>
-            <h2 class="mb-1.5 font-medium">New Todo</h1>
-                <livewire:components.todo.create />
-        </header>
-        <section class="mt-6">
-            <h2 class="mb-1.5 font-medium">To be done</h1>
-                <livewire:components.todo.table :where="['done', 0]" />
-        </section>
-        <section class="mt-6">
-            <h2 class="mb-1.5 font-medium">Done</h2>
-            <livewire:components.todo.table :where="['done', 1]" />
-        </section>
-    </main>
+    @volt('index')
+        <main class="py-12 max-w-xl mx-auto">
+            <header>
+                <h2 class="mb-1.5 font-medium">New Todo</h1>
+                    <livewire:components.todo.create />
+            </header>
+            <section class="mt-6">
+                <h2 class="mb-1.5 font-medium">To be done ({{ $open->count() }})</h1>
+                    <livewire:components.todo.table :data="$open" />
+            </section>
+            <section class="mt-6">
+                <h2 class="mb-1.5 font-medium">Done ({{ $done->count() }})</h2>
+                <livewire:components.todo.table :data="$done" />
+            </section>
+        </main>
+    @endvolt
 @endsection
