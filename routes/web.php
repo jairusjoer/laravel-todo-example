20b344964f96f83cf1616a93e39604ebb1bdfedd
Jairus Joer <hello@jairusjoer.com>
2024-07-14 20:55:42 +0200

    Remove obsolete routes

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,3 @@
 <?php
 
-use App\Http\Controllers\TodosController;
-use App\Models\Todo;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('pages/index', ['todos' => Todo::all()]);
-});
